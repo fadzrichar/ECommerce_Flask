b9d2caba7f326d0c838a1c2300dd07d85be804ca
Charisma Fadzri Triprakoso <fadzri@alterra.id>
2020-01-23 09:24:47 +0700

    back

--- blueprints/__init__.py
+++ blueprints/__init__.py
@@ -23,8 +23,8 @@ try:
     if env == 'testing':
         app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://{uname}:{pwd}@0.0.0.0:3306/ECommerce_test".format(uname=uname, pwd=pwd)
     else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:alta1234@ecommerce.czyclfjzsfbe.ap-southeast-1.rds.amazonaws.com:3306/ecommerce'
         # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://{uname}:{pwd}@{db_endpoint}:3306/{db_dev}".format(uname=uname, pwd=pwd, db_dev=db_dev, db_endpoint=db_endpoint)
-		app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:alta1234@ecommerce.czyclfjzsfbe.ap-southeast-1.rds.amazonaws.com:3306/ecommerce'
 except Exception as e:
     raise e
 
