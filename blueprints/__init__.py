f813ea5fe1de709cc840cadae0c344763a4ad669
Charisma Fadzri Triprakoso <fadzri@alterra.id>
2020-01-23 08:30:55 +0700

    trying added variable env

--- blueprints/__init__.py
+++ blueprints/__init__.py
@@ -1,3 +1,8 @@
+from blueprints.transaction.resources import bp_transaction
+from blueprints.cart.resources import bp_cart
+from blueprints.product.resources import bp_product
+from blueprints.user.resources import bp_user
+from blueprints.auth import bp_auth
 import json, os
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
@@ -12,15 +17,20 @@ app = Flask(__name__)
 app.config['APP_DEBUG'] = True
 CORS(app)
 
-try:
-	env = os.environ.get('FLASK_ENV', 'development')
-	if env == 'testing':
-		app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:alta1234@0.0.0.0:3306/ECommerce_test'
-	else:
-		app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:alta1234@ecommerce.czyclfjzsfbe.ap-southeast-1.rds.amazonaws.com:3306/ecommerce'
-except Exception as e:
-	raise e
+uname = os.environ["THIS_UNAME"]
+pwd = os.environ["THIS_PWD"]
+# db_test = os.environ["THIS_DB_TEST"]
+db_dev = os.environ["THIS_DB_DEV"]
+db_endpoint = os.environ["THIS_DB_ENDPOINT"]
 
+try:
+    env = os.environ.get('FLASK_ENV', 'development')
+    if env == 'testing':
+        app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://{uname}:{pwd}@0.0.0.0:3306/ECommerce_test".format(uname=uname, pwd=pwd)
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://{uname}:{pwd}@{db_endpoint}:3306/{db_dev}".format(uname=uname, pwd=pwd, db_dev=db_dev, db_endpoint=db_endpoint)
+except Exception as e:
+    raise e
 
 
 # sqlalchemy config
@@ -34,80 +44,79 @@ app.config['JWT_SECRET_KEY'] = 'FaDzRiChArIsMa'
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=30)
 jwt = JWTManager(app)
 
-#jwt custom decorator admin
+# jwt custom decorator admin
+
+
 def internal_required(fn):
-	@wraps(fn)
-	def wrapper(*args,**kwargs):
-		verify_jwt_in_request()
-		claims = get_jwt_claims()
-		if claims['username'] != 'admin':
-			return {'status': 'FORBIDDEN', 'message': 'Admin Only'}, 403
-		else:
-			return fn(*args, **kwargs)
-	return wrapper
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        verify_jwt_in_request()
+        claims = get_jwt_claims()
+        if claims['username'] != 'admin':
+            return {'status': 'FORBIDDEN', 'message': 'Admin Only'}, 403
+        else:
+            return fn(*args, **kwargs)
+    return wrapper
 
 ##############################
 # SQLAlchemy Database config #
 ##############################
 
+
 db = SQLAlchemy(app)
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+
 @app.after_request
 def after_request(response):
-	try:
-		requestData = request.get_json()
-	except Exception as e:
-		requestData = request.args.to_dict()
-	# if request.method == 'GET':
-	if response.status_code == 200:
-		app.logger.info("REQUEST_LOG\t%s",
-			json.dumps({
-				'status_code': response.status_code,
-				'method': request.method,
-				'code': response.status,
-				'uri': request.full_path,
-				'request':request.args.to_dict(),
-				'response':json.loads(response.data.decode('utf8'))
-				})
-				)
-	elif response.status_code == 404:
-		app.logger.warning("REQUEST_LOG\t%s",
-			json.dumps({
-				'status_code': response.status_code,
-				'method': request.method,
-				'code': response.status,
-				'uri': request.full_path,
-				'request':request.args.to_dict(),
-				'response':json.loads(response.data.decode('utf8'))
-				})
-				)
-	else:
-		app.logger.error("REQUEST_LOG\t%s",
-			json.dumps({
-				'status_code': response.status_code,
-				'method': request.method,
-				'code': response.status,
-				'uri': request.full_path,
-				'request':request.args.to_dict(),
-				'response':json.loads(response.data.decode('utf8'))
-				})
-				)
+    try:
+        requestData = request.get_json()
+    except Exception as e:
+        requestData = request.args.to_dict()
+    # if request.method == 'GET':
+    if response.status_code == 200:
+        app.logger.info("REQUEST_LOG\t%s",
+                        json.dumps({
+                            'status_code': response.status_code,
+                            'method': request.method,
+                            'code': response.status,
+                            'uri': request.full_path,
+                            'request': request.args.to_dict(),
+                            'response': json.loads(response.data.decode('utf8'))
+                        })
+                        )
+    elif response.status_code == 404:
+        app.logger.warning("REQUEST_LOG\t%s",
+                           json.dumps({
+                               'status_code': response.status_code,
+                               'method': request.method,
+                               'code': response.status,
+                               'uri': request.full_path,
+                               'request': request.args.to_dict(),
+                               'response': json.loads(response.data.decode('utf8'))
+                           })
+                           )
+    else:
+        app.logger.error("REQUEST_LOG\t%s",
+                         json.dumps({
+                             'status_code': response.status_code,
+                             'method': request.method,
+                             'code': response.status,
+                             'uri': request.full_path,
+                             'request': request.args.to_dict(),
+                             'response': json.loads(response.data.decode('utf8'))
+                         })
+                         )
 
-	return response
+    return response
 
 
 #####################
 # IMPORT BLUEPRINTS #
 #####################
 
-from blueprints.auth import bp_auth
-from blueprints.user.resources import bp_user
-from blueprints.product.resources import bp_product
-from blueprints.cart.resources import bp_cart
-from blueprints.transaction.resources import bp_transaction
 
 app.register_blueprint(bp_auth, url_prefix='/login')
 app.register_blueprint(bp_user, url_prefix='/users')
--- dockerfile
+++ dockerfile
@@ -3,6 +3,13 @@ MAINTAINER Your Name "fadzri@alterra.id"
 RUN mkdir -p /demo
 COPY . /demo
 RUN pip install -r /demo/requirements.txt
+# Import variable env
+ENV THIS_UNAME=THIS_UNAME_VALUE
+ENV THIS_PWD=THIS_PWD_VALUE
+# ENV THIS_DB_TEST=THIS_DB_TEST_VALUE
+ENV THIS_DB_DEV=THIS_DB_DEV_VALUE
+ENV THIS_DB_ENDPOINT=THIS_DB_ENDPOINT_VALUE
+
 WORKDIR /demo
 ENTRYPOINT [ "python" ]
 CMD [ "app.py" ]
